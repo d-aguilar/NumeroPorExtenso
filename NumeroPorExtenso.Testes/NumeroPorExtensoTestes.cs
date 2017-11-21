2b8a8c25e08f04913facf4b191292010dede95a4
Diogo Aguilar <diogoaguilar89@gmail.com>
2017-11-21 21:19:33 -0200

    alterações

---
+++ NumeroPorExtenso.Testes/NumeroPorExtensoTestes.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumeroPorExtenso.Testes
+{
+    [TestClass]
+    public class NumeroPorExtensoTestes
+    {
+        [TestMethod]
+        public void Testar_Numero_Por_Extenso()
+        {
+            Assert.AreEqual(null, null);
+        }
+    }
+}
