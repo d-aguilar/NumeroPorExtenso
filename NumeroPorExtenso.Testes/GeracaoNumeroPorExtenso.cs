8e520fe2a5cc32726422c6240bc2ebbb2d0a510a
Diogo Aguilar <diogoaguilar89@gmail.com>
2017-11-22 00:13:00 -0200

    Alterações

---
+++ NumeroPorExtenso.Testes/GeracaoNumeroPorExtenso.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace NumeroPorExtenso.Testes
+{
+    public class GeracaoNumeroPorExtenso
+    {
+        [Fact]
+        public void Testar_Geraracao_Numero_Por_Extenso()
+        {
+            var teste = new Core.GeradorNumeroPorExtenso();
+            Assert.Equal(null, null);
+        }
+    }
+}
